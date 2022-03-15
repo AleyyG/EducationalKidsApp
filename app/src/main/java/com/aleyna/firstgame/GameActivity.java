b11fe9a7823c72de8b313e95031cb51647565a88
Aleyna Güneyli <aleynaa.guneylii@gmail.com>
2022-03-15 23:38:46 +0300

    Educational Kids App

--- app/src/main/java/com/aleyna/firstgame/GameActivity.java
+++ app/src/main/java/com/aleyna/firstgame/GameActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -19,6 +20,15 @@ public class GameActivity extends AppCompatActivity {
         final LottieAnimationView numbersGame = findViewById(R.id.numbersGame);
         final LottieAnimationView puzzleGame = findViewById(R.id.puzzleGame);
         final LottieAnimationView foodGame = findViewById(R.id.foodGame);
+        final Button mathGame = findViewById(R.id.mathGame);
+
+        mathGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mathGameScene = new Intent(GameActivity.this,MathGameActivity.class);
+                startActivity(mathGameScene);
+            }
+        });
 
         colorGame.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/aleyna/firstgame/MathGameActivity.java
@@ -0,0 +1,14 @@
+package com.aleyna.firstgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MathGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_game);
+    }
+}
